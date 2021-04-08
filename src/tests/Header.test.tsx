d285eb047d77292241c5f53d4bb3522b87b2c834
MiHuYo <sweng23@wisc.edu>
2021-04-07 21:44:10 -0500

    Created test for Header

--- src/tests/Header.test.tsx
+++ src/tests/Header.test.tsx
@@ -1,18 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { cleanup, render } from "@testing-library/react";
-import Reviews from "../components/DashboardReviews";
+import Header from "../components/Header";
 
 afterEach(cleanup);
 
 // eslint-disable-next-line jest/expect-expect
 it("renders without crashing", () => {
   const div = document.createElement("div");
-  ReactDOM.render(<Reviews />, div);
+  ReactDOM.render(<Header title="Test" />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
 
-it("renders Reviews title correcetly", () => {
-  const { getByTestId } = render(<Reviews />);
-  expect(getByTestId("reviews")).toHaveTextContent("Reviews");
+it("renders Header title correctly", () => {
+  const { getByTestId } = render(<Header title="Test" />);
+  expect(getByTestId("Header")).toHaveTextContent("Test");
 });
